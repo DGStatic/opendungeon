b9a27214453727b56c7b1e84bf93fa079855285d
Ian Shakespeare <70654892+ian-shakespeare@users.noreply.github.com>
2026-07-15 00:12:32 +0900

    Svelte (#29)
    * migrate to svelte
    
    * fix linter errors
    
    * install playwright in actions
    
    * fix bad workflow file
    
    * remove vitest examples
    
    * favicon
    
    * switch from spa to ssg

---
+++ web/src/routes/sign-in/+page.svelte
@@ -0,0 +1,38 @@
+<script lang="ts">
+  import { goto } from "$app/navigation";
+  import { resolve } from "$app/paths";
+  import { signIn } from "$lib/api.svelte";
+
+  let email = $state("");
+  let password = $state("");
+
+  async function handleSubmit(event: SubmitEvent) {
+    event.preventDefault();
+
+    const res = await signIn(email, password);
+    if (res.ok) {
+      await goto(resolve("/dashboard"));
+      return;
+    }
+
+    // TODO: toast or something
+    console.error(res.error.message);
+  }
+</script>
+
+<svelte:head>
+  <title>Sign In - OpenDungeon</title>
+</svelte:head>
+
+<h1>Sign In</h1>
+<form onsubmit={handleSubmit} class="grid">
+  <label>
+    Email
+    <input bind:value={email} type="email" />
+  </label>
+  <label>
+    Password
+    <input bind:value={password} type="password" />
+  </label>
+  <input type="submit" value="Sign In" class="cursor-pointer" />
+</form>
