f166c16266aee2b42418576cf619b06bf2a1da45
Ian Shakespeare <70654892+ian-shakespeare@users.noreply.github.com>
2026-08-02 22:24:21 -0600

    Brush Size (#43)
    * brush makes me want to die
    
    * added brush size slider

--- web/src/lib/components/BrushToolOptionMenu.svelte
+++ web/src/lib/components/BrushToolOptionMenu.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-  import { DEFAULT_VIEW_MODE, type BrushTool } from "$lib/game/level-editor";
+  import { DEFAULT_VIEW_MODE, type BrushTool, type BrushWeightTool } from "$lib/game/level-editor";
   import StyledCard from "$lib/components/StyledCard.svelte";
   import StyledButton from "./StyledButton.svelte";
 
@@ -31,12 +31,32 @@
 </script>
 
 <StyledCard class="grid gap-3 px-4 py-3 pointer-events-auto">
+  <div>
+    <label>
+      Radius
+      <input
+        type="range"
+        min={1}
+        max={10}
+        defaultValue={0}
+        onchange={(event) => {
+          if (!event.target) {
+            return;
+          }
+
+          const target = event.target as HTMLInputElement;
+          const radius = Number(target.value);
+          brush = { ...brush, radius };
+        }}
+      />
+    </label>
+  </div>
   <div class="flex gap-2">
     {#each brushModes as brushMode, i (i)}
       <StyledButton
         mode={brushMode.selected ? "primary" : "outline"}
         onclick={() => {
-          brush = brushMode.tool;
+          brush = { ...brushMode.tool, radius: brush.radius };
           viewMode = brushMode.viewMode;
         }}
         label={brushMode.label}
@@ -53,7 +73,7 @@
               id={`${weight}-weight-select`}
               type="radio"
               checked={brush.weight === weight}
-              onchange={() => (brush = { type: "weightbrush", weight })}
+              onchange={() => (brush = { ...brush, weight } as BrushWeightTool)}
             />
             <label for={`${weight}-weight-select`}>{label}</label>
           </div>
